24414a18cdc1a34d1879510a53b55323c1d3adb5
luis antonio amaya villalta <amayantonio@hotmail.com>
2022-05-06 05:43:06 -0600

    Firebase Autentic User Kotlin

---
+++ app/src/main/java/com/example/autenticuser/SignUpActivity.kt
@@ -0,0 +1,104 @@
+package com.example.autenticuser
+
+import android.R.attr.password
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
+class SignUpActivity : AppCompatActivity() {
+
+    lateinit var etEmail: EditText
+    lateinit var etConfPass: EditText
+    private lateinit var etPass: EditText
+    private lateinit var btnSignUp: Button
+    lateinit var tvRedirectLogin: TextView
+
+    // create Firebase authentication object
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+
+        // View Bindings
+        etEmail = findViewById(R.id.etSEmailAddress)
+        etConfPass = findViewById(R.id.etSConfPassword)
+        etPass = findViewById(R.id.etSPassword)
+        btnSignUp = findViewById(R.id.btnSSigned)
+        tvRedirectLogin = findViewById(R.id.tvRedirectLogin)
+
+        // Initialising auth object
+        auth = Firebase.auth
+
+        btnSignUp.setOnClickListener {
+            signUpUser()
+        }
+
+        // switching from signUp Activity to Login Activity
+        tvRedirectLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun signUpUser() {
+        val email = etEmail.text.toString()
+        val pass = etPass.text.toString()
+        val confirmPassword = etConfPass.text.toString()
+
+        // check pass
+        if (email.isBlank() || pass.isBlank() || confirmPassword.isBlank()) {
+            Toast.makeText(this, "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (pass != confirmPassword) {
+            Toast.makeText(this, "Password and Confirm Password do not match", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+
+
+
+
+        auth.createUserWithEmailAndPassword(email, pass)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                   // updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                  //  updateUI(null)
+                }
+            }
+
+
+
+
+
+
+
+
+    }
+}
